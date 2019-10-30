04b40bf5c912b45286d0c4638830b7e83eaa3aed
kj_cho <kj_cho@idealidea.co.kr>
2019-10-30 22:21:34 +0900

    로그인 버튼 이벤트 연습까지. - String.format 활용 문제

--- app/src/main/java/com/tjoeun/loginpractice/MainActivity.kt
+++ app/src/main/java/com/tjoeun/loginpractice/MainActivity.kt
@@ -2,11 +2,23 @@ package com.tjoeun.loginpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        loginBtn.setOnClickListener {
+            var inputId = idEdt.text.toString()
+            var inputPw = pwEdt.text.toString()
+
+            Toast.makeText(this, String.format("ID : %s, PW : %s", inputId, inputPw), Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 }
